e7f27276fa2643bcab2b0c2c0a7d7ce4810d9464
Calum Harris <harris.calum@gmail.com>
2016-04-29 12:00:20 +0100

    Add basic UI

---
+++ OnTheMap_P3_iOS_NanoDegree/ViewController/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  OnTheMap_P3_iOS_NanoDegree
+//
+//  Created by Calum Harris on 29/04/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  
+  @IBOutlet weak var loginButton: UIButton!
+  @IBOutlet weak var signUpButton: UIButton!
+  
+  
+}
