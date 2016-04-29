f1ca2fb05126adbda142884d19057af0990f3027
Calum Harris <harris.calum@gmail.com>
2016-04-29 12:58:14 +0100

    add more UI with outlets and viewController classes

---
+++ OnTheMap_P3_iOS_NanoDegree/ViewController/InfoPostingViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InfoPostingViewController.swift
+//  OnTheMap_P3_iOS_NanoDegree
+//
+//  Created by Calum Harris on 29/04/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import UIKit
+
+class InfoPostingViewController: UIViewController {
+  
+  
+  @IBOutlet weak var whereAreyouStudying1: UILabel!
+  @IBOutlet weak var whereAreYouStudying2: UILabel!
+  @IBOutlet weak var whereAreYouStudying3: UILabel!
+  
+  @IBOutlet weak var cancelBarButton: UIBarButtonItem!
+  
+  @IBOutlet weak var webAddressTextField: UITextField!
+  @IBOutlet weak var locationTextField: UITextField!
+  
+  @IBOutlet weak var submitButton: UIButton!
+  @IBOutlet weak var findOnMapButton: UIButton!
+  
+  
+}
---
+++ OnTheMap_P3_iOS_NanoDegree/ViewController/TabBarMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarMapViewController.swift
+//  OnTheMap_P3_iOS_NanoDegree
+//
+//  Created by Calum Harris on 29/04/2016.
+//  Copyright © 2016 Calum Harris. All rights reserved.
+//
+
+import UIKit
+
+class TabBarMapViewController: UIViewController {
+  
+  
+  
+  
+}
